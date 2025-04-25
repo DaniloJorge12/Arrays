bb310d52c1e485d846c8fdd9943260ca5c87d3b8
Danilo J. <danilojmini2@gmail.com>
2025-04-25 09:30:36 -0300

    Exercício 1

---
+++ Ex01.js
@@ -0,0 +1,5 @@
+//Crie um array com os nomes dos 5 jogadores que você colocaria no seu time
+//dos sonhos.
+
+let jogadores = ['Messi', 'Cristiano Ronaldo', 'Neymar', 'Pelé', 'Thiago Ferreira', 'Eduardo Correia']
+console.log(jogadores)
