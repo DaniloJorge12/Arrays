6514fba37003ca7e6f6ed610ad899522e2a8c37d
Danilo J. <danilojmini2@gmail.com>
2025-04-25 09:57:55 -0300

    Exercício 3

---
+++ Ex03.js
@@ -0,0 +1,10 @@
+//Você criou uma lista com seus 3 melhores amigos, mas escreveu o nome de
+//um errado. Corrija!
+//Somente 3 professor? aaaaaaa
+
+let listadeamigos = ['mílio', 'Pedro', 'Victor']
+let amigos = listadeamigos.length
+
+listadeamigos[0] = 'Hemílio'
+
+console.log(listadeamigos)
