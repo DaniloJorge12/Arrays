8cc35093dace69aeda1b5f68ed612389b65d3b68
Danilo J. <danilojmini2@gmail.com>
2025-04-25 11:19:52 -0300

    Exercício 9

---
+++ Ex09.js
@@ -0,0 +1,5 @@
+//Crie uma lista de 6 frutas e mostre quantas tem no total.
+
+let frutas = ['Banana', 'Mamão', 'Morango', 'Pera', 'Maça', 'Limão']
+let quantidade = frutas.length
+console.log('Quantidade de frutas: ' + quantidade)
