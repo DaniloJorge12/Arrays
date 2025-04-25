af65162df6aae23e59f3d727177f4f90b8f64799
Danilo J. <danilojmini2@gmail.com>
2025-04-25 11:12:25 -0300

    Exercício 7

---
+++ Ex08.js
@@ -0,0 +1,6 @@
+//Crie um array com idades e mostre só as que forem 18 ou mais.
+
+const idades = ['37', '41', '16', '15']
+const maioresdeidade = idades.filter(idade => idade >= 18);
+
+console.log(maioresdeidade); //
