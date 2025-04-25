13fd8f31cc6fad78dbb4ad5ef3fb2368e1cfc0e4
Danilo J. <danilojmini2@gmail.com>
2025-04-25 10:26:12 -0300

    Exercício 4

---
+++ Ex04.js
@@ -0,0 +1,5 @@
+//Crie uma lista com 3 comidas e depois adicione mais uma no final.
+let listadecomida = ['Melão', 'Hamburguer', 'Esfirra']
+listadecomida.push('Banana')
+
+console.log(listadecomida)
