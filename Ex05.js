69b7ea72b7e8785a92913358c6ac9c12b598da1a
Danilo J. <danilojmini2@gmail.com>
2025-04-25 10:30:45 -0300

    Exercício 5

---
+++ Ex05.js
@@ -0,0 +1,6 @@
+//Use .pop() para remover o último item de uma lista de compras.
+
+let listadecompras = ['Cachorro Quente', 'Maça', 'Mortadela']
+listadecompras.pop(2)
+
+console.log(listadecompras)
